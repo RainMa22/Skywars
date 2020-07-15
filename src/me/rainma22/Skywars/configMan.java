a13212ce141c59935af0a6c62c616a54f76692e0
d2rai <d2rain@126.com>
2020-07-15 15:19:48 -0700

    config setting up

--- src/me/rainma22/Skywars/configMan.java
+++ src/me/rainma22/Skywars/configMan.java
@@ -4,21 +4,48 @@ import org.bukkit.configuration.file.YamlConfiguration;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class configMan {
     private Main plugin;
+    ArrayList<config> configs=new ArrayList<>(0);
     public configMan(Main p){
         plugin=p;
     }
+    public void addConfig(String s){
+        configs.add(new config(new File(s)));
+    }
+    public config getConfig(String s){
+        for(config c:configs){
+            if(c.file.getName().equalsIgnoreCase(s)){
+                return c;
+            }
+        }
+        return null;
+    }
+
 }
 class config{
     public YamlConfiguration yaml;
     public File file;
     public config(File f){
-        file=f;
-        yaml= new YamlConfiguration();
         try {
+        file=f;
+        file.getParentFile().mkdirs();
+        file.createNewFile();
+        yaml= new YamlConfiguration();
             yaml.save(f);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public Object load(String p){
+        return yaml.get(p);
+    }
+    public void save(String p,String c){
+        try {
+        yaml.set(p,c);
+            yaml.save(file);
         } catch (IOException e) {
             e.printStackTrace();
         }
