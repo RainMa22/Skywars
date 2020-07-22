42fc5cecf690df32ddb35612c43294a32515b0bb
rainma22 <d2rain@126.com>
2020-07-22 14:25:57 +0000

    WorldMan function

--- src/me/rainma22/Skywars/Worldman.java
+++ src/me/rainma22/Skywars/Worldman.java
@@ -1,11 +1,25 @@
 package me.rainma22.Skywars;
 
-import java.io.File;
+
+import org.bukkit.WorldCreator;
+import org.bukkit.WorldType;
 
 public class Worldman {
     private Main plugin;
     public Worldman(Main plugin){
         this.plugin=plugin;
     }
+    public void genNewWorld(String name){
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                WorldCreator wc= new WorldCreator(name);
+                wc.generateStructures(false);
+                wc.type(WorldType.CUSTOMIZED);
+                wc.generatorSettings("0:1");
+                wc.createWorld();
+            }
+        }).start();
 
+    }
 }
